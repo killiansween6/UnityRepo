0ebf504dcc9ec9a9ef97eb05810df0a58c2c317f
Student Killian Sweeney (TL_KCOMP_D_Y1) <t00287926@mymtu.ie>
2025-10-16 10:56:08 +0100

    Used unity, wouldn't reccommend, 0/5 baked hams

---
+++ My project (2)/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += 1.0f * Vector3.up * Time.deltaTime;
+
+        }
+
+    }
+}
